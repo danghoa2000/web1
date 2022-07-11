08358fd05ea9a19a9273ba82a94363cbaea3a4b6
Cao Hai <hai.cx@beetechsoft.vn>
2022-07-11 10:16:17 +0700

    uploads

---
+++ resources/views/pages/send_mail.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Gửi mail google</title>
+</head>
+<body>
+	<h1>Xin chào : {{$shipping_arr['customer_name']}}</h1>
+	<h4>Xác nhận đặt hàng </h4>
+	<span>Cảm ơn bạn đã đặt hàng từ shop của chúng tôi</span><br>
+	<span>Sẽ có nhân viên của shop gọi điện xác nhận đơn hàng của bạn </span><br>
+	<span>Xin chân thành cảm ơn</span>
+</body>
+</html>
